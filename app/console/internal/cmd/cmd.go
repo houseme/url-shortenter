58e3de7eff4d60d7ba9aaf1718b7d3a4a126857c
houseme <housemecn@gmail.com>
2022-07-26 18:28:58 +0800

    feat: improve readme.md (#4)

--- app/console/internal/cmd/cmd.go
+++ app/console/internal/cmd/cmd.go
@@ -17,7 +17,7 @@ var (
 		Brief: "start http server",
 		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
 			s := g.Server()
-			s.Group("/", func(group *ghttp.RouterGroup) {
+			s.Group("/console/api", func(group *ghttp.RouterGroup) {
 				group.Middleware(ghttp.MiddlewareHandlerResponse)
 				group.Bind(
 					controller.Echo,
