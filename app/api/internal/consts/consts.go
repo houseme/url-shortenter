7386c29523d9246ac6cad7d778cc88bbdbb7f0a2
houseme <housemecn@gmail.com>
2022-09-29 00:33:30 +0800

    feat: create copyright

--- app/api/internal/consts/consts.go
+++ app/api/internal/consts/consts.go
@@ -1,3 +1,10 @@
+// Copyright Url-Shortenter Author(https://houseme.github.io/url-shortenter/). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://github.com/houseme/url-shortenter.
+
+// Package consts .
 package consts
 
 const (
