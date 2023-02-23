2c23a30e4eb12acd536b0d302106b231de9c6b69
houseme <housemecn@gmail.com>
2023-02-23 09:18:25 +0800

    feat: improve code

--- app/console/internal/controller/auth.go
+++ app/console/internal/controller/auth.go
@@ -10,12 +10,10 @@ import (
 	"context"
 
 	"github.com/gogf/gf/v2/errors/gerror"
-	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/gtrace"
 
 	v1 "github.com/houseme/url-shortenter/app/console/api/v1"
 	"github.com/houseme/url-shortenter/app/console/internal/service"
-	"github.com/houseme/url-shortenter/utility/helper"
 )
 
 type cAuth struct {
@@ -29,10 +27,8 @@ func (c *cAuth) CreateAccessToken(ctx context.Context, req *v1.CreateAccessToken
 	ctx, span := gtrace.NewSpan(ctx, "tracing-controller-auth-CreateAccessToken")
 	defer span.End()
 
-	var logger = helper.Helper().Logger(ctx)
 	res = &v1.CreateAccessTokenRes{}
 	if res.CreateAccessTokenOutput, err = service.Auth().CreateAccessToken(ctx, req.CreateAccessTokenInput); err != nil {
-		g.Log(logger).Error(ctx, "auth-CreateAccessToken err:", err)
 		err = gerror.Wrap(err, "auth-CreateAccessToken failed")
 	}
 	return
@@ -43,10 +39,8 @@ func (c *cAuth) Authorization(ctx context.Context, req *v1.AuthReq) (res *v1.Aut
 	ctx, span := gtrace.NewSpan(ctx, "tracing-controller-auth-authorization")
 	defer span.End()
 
-	var logger = helper.Helper().Logger(ctx)
 	res = &v1.AuthRes{}
 	if res.AuthOutput, err = service.Auth().Authorization(ctx, req.AuthInput); err != nil {
-		g.Log(logger).Error(ctx, "auth-authorization err:", err)
 		err = gerror.Wrap(err, "auth-authorization failed")
 	}
 	return
