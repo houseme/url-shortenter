473aedad03f2dd74ddc0bd4e45891dca7d9fd6b7
houseme <housemecn@gmail.com>
2023-07-03 15:19:47 +0800

    feat: improve for trace

--- internal/tracing/attribute.go
+++ internal/tracing/attribute.go
@@ -11,8 +11,7 @@ import (
 
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/net/gtrace"
-	"github.com/gogf/gf/v2/util/gconv"
-	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -20,21 +19,21 @@ const telemetrySDKName = "opentelemetry"
 
 // SetAttributes .set tracing attributes
 func SetAttributes(r *ghttp.Request, span *gtrace.Span) {
-	span.SetAttributes(semconv.HTTPURLKey.String(r.URL.Path))
-	span.SetAttributes(semconv.HTTPMethodKey.String(r.Method))
-	span.SetAttributes(semconv.HTTPHostKey.String(r.GetHost()))
-	span.SetAttributes(semconv.HTTPSchemeKey.String(r.Proto))
-	span.SetAttributes(semconv.HTTPStatusCodeKey.String(gconv.String(r.Response.Status)))
-	span.SetAttributes(semconv.HTTPUserAgentKey.String(r.UserAgent()))
+	span.SetAttributes(semconv.HTTPURL(r.URL.Path))
+	span.SetAttributes(semconv.HTTPMethod(r.Method))
+	span.SetAttributes(semconv.NetHostName(r.GetHost()))
+	span.SetAttributes(semconv.HTTPScheme(r.Proto))
+	span.SetAttributes(semconv.HTTPStatusCode(r.Response.Status))
+	span.SetAttributes(semconv.UserAgentOriginal(r.UserAgent()))
 }
 
 // CommonEventOption .
 func CommonEventOption(_ context.Context, namespace string) trace.SpanStartEventOption {
 	return trace.WithAttributes(
-		semconv.ServiceNamespaceKey.String(namespace),
-		semconv.TelemetrySDKNameKey.String(telemetrySDKName),
-		semconv.TelemetrySDKVersionKey.String("1.0.0"),
-		semconv.TelemetryAutoVersionKey.String("1.0.0"),
+		semconv.ServiceNamespace(namespace),
+		semconv.TelemetrySDKName(telemetrySDKName),
+		semconv.TelemetrySDKVersion("1.0.0"),
+		semconv.TelemetryAutoVersion("1.0.0"),
 		semconv.TelemetrySDKLanguageGo,
 	)
 }
--- internal/tracing/tracing.go
+++ internal/tracing/tracing.go
@@ -11,26 +11,54 @@ import (
 	"strings"
 
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/net/gipv4"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"google.golang.org/grpc"
 
 	"github.com/houseme/url-shortenter/utility/env"
 )
 
 const (
 	tracerHostnameTagKey = "hostname"
+	traceJaeger          = "jaeger"
+	traceOtlpGRPC        = "otlpgrpc"
 )
 
 // InitJaeger initializes and registers jaeger to global TracerProvider.
 //
 // The output parameter `flush` is used for waiting exported trace spans to be uploaded,
 // which is useful if your program is ending, and you do not want to lose recent spans.
-func InitJaeger(serviceName, endpoint, version, environment, hostIP string) (*trace.TracerProvider, error) {
-	var endpointOption jaeger.EndpointOption
+func InitJaeger(serviceName, endpoint, version, environment string) (*trace.TracerProvider, error) {
+	var (
+		intranetIPArray, err = gipv4.GetIntranetIpArray()
+		hostIP               = "NoHostIpFound"
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(intranetIPArray) == 0 {
+		if intranetIPArray, err = gipv4.GetIpArray(); err != nil {
+			return nil, err
+		}
+	}
+	if len(intranetIPArray) > 0 {
+		hostIP = intranetIPArray[0]
+	}
+
+	var (
+		endpointOption jaeger.EndpointOption
+		exp            *jaeger.Exporter
+	)
 	if strings.HasPrefix(endpoint, "http") {
 		// HTTP.
 		endpointOption = jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(endpoint))
@@ -38,8 +66,7 @@ func InitJaeger(serviceName, endpoint, version, environment, hostIP string) (*tr
 		// UDP.
 		endpointOption = jaeger.WithAgentEndpoint(jaeger.WithAgentHost(endpoint))
 	}
-	exp, err := jaeger.New(endpointOption)
-	if err != nil {
+	if exp, err = jaeger.New(endpointOption); err != nil {
 		return nil, err
 	}
 
@@ -49,10 +76,10 @@ func InitJaeger(serviceName, endpoint, version, environment, hostIP string) (*tr
 		// Record information about this application in a Resource.
 		trace.WithResource(resource.NewWithAttributes(
 			semconv.SchemaURL,
-			semconv.ServiceNameKey.String(serviceName+"-"+environment),
-			semconv.ServiceVersionKey.String(version),
-			semconv.DeploymentEnvironmentKey.String(environment),
-			semconv.HostNameKey.String(hostIP),
+			semconv.ServiceName(serviceName+"-"+environment),
+			semconv.ServiceVersion(version),
+			semconv.DeploymentEnvironment(environment),
+			semconv.HostName(hostIP),
 			attribute.String(tracerHostnameTagKey, hostIP),
 		)),
 	)
@@ -65,9 +92,81 @@ func InitTracer(ctx context.Context, serviceName string) {
 	if appEnv, err := env.New(ctx); err != nil {
 		g.Log().Fatal(ctx, err)
 	} else {
-		_, err = InitJaeger(serviceName, appEnv.JaegerEndpoint(ctx), appEnv.Version(ctx), appEnv.Environment(ctx), appEnv.HostIP(ctx))
+		if appEnv.TraceType(ctx) == traceJaeger {
+			_, err = InitJaeger(serviceName, appEnv.JaegerEndpoint(ctx), appEnv.Version(ctx), appEnv.Environment(ctx))
+		}
+		if appEnv.TraceType(ctx) == traceOtlpGRPC {
+			_, err = Init(serviceName, appEnv.Endpoint(ctx), appEnv.TraceToken(ctx), appEnv.Version(ctx), appEnv.Environment(ctx))
+		}
 		if err != nil {
 			g.Log().Fatal(ctx, err)
 		}
 	}
+}
+
+// Init initializes and registers `otlpgrpc` to global TracerProvider.
+//
+// The output parameter `Shutdown` is used for waiting exported tracing spans to be uploaded,
+// which is useful if your program is ending, and you do not want to lose recent spans.
+func Init(serviceName, endpoint, traceToken, version, environment string) (*trace.TracerProvider, error) {
+	// Try retrieving host ip for tracing info.
+	var (
+		intranetIPArray, err = gipv4.GetIntranetIpArray()
+		hostIP               = "NoHostIpFound"
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(intranetIPArray) == 0 {
+		if intranetIPArray, err = gipv4.GetIpArray(); err != nil {
+			return nil, err
+		}
+	}
+	if len(intranetIPArray) > 0 {
+		hostIP = intranetIPArray[0]
+	}
+
+	traceClient := otlptracegrpc.NewClient(
+		otlptracegrpc.WithInsecure(),
+		otlptracegrpc.WithEndpoint(endpoint), // Replace the otel Agent Addr with the access point obtained in the prerequisite。
+		otlptracegrpc.WithHeaders(map[string]string{"Authentication": traceToken}),
+		otlptracegrpc.WithDialOption(grpc.WithBlock()),
+	)
+	ctx := context.Background()
+	traceExp, err := otlptrace.New(ctx, traceClient)
+	if err != nil {
+		return nil, err
+	}
+	res, err := resource.New(ctx,
+		resource.WithFromEnv(),
+		resource.WithProcess(),
+		resource.WithTelemetrySDK(),
+		resource.WithHost(),
+		resource.WithAttributes(
+			// The name of the service displayed on the traceback end。
+			semconv.ServiceNameKey.String(serviceName+"-"+environment),
+			semconv.ServiceVersionKey.String(version),
+			semconv.DeploymentEnvironmentKey.String(environment),
+			semconv.HostNameKey.String(hostIP),
+			attribute.String(tracerHostnameTagKey, hostIP),
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	bsp := trace.NewBatchSpanProcessor(traceExp)
+	tracerProvider := trace.NewTracerProvider(
+		trace.WithSampler(trace.AlwaysSample()),
+		trace.WithResource(res),
+		trace.WithSpanProcessor(bsp),
+	)
+
+	// Set the global propagator to traceContext (not set by default).
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+	otel.SetTracerProvider(tracerProvider)
+
+	return tracerProvider, nil
 }
