0b7a84037fee65817827bd2e768afb23930541b6
houseme <housemecn@gmail.com>
2023-08-21 18:40:28 +0800

    fix

--- app/front/internal/model/home.go
+++ app/front/internal/model/home.go
@@ -9,7 +9,7 @@ package model
 
 // HomeInput is a struct for input data
 type HomeInput struct {
-	Short      string `v1:"required|min-length:2|max-length:256#请输入短链接|最小长度为{min}|最大长度为{max}" json:"short" dc:"shortUrl 短链接"`
+	Short      string `v:"required|min-length:2|max-length:256#请输入短链接 | 最小长度为{min}|最大长度为{max}" json:"short" dc:"shortUrl 短链接"`
 	RawQuery   string `json:"rawQuery" dc:"rawQuery 原始查询"`
 	ClientIP   string `json:"clientIp"`
 	UserAgent  string `json:"userAgent"`
