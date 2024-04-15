bfc0a4bb761c2f99847dab2ce7ccc9daec2ac6a6
houseme <housemecn@gmail.com>
2024-04-15 12:31:37 +0800

    remove code (#1208)

--- internal/crawler/colly.go
+++ internal/crawler/colly.go
@@ -7,24 +7,24 @@
 // Package crawler is a utility package for crawler.
 package crawler
 
-import (
-	"fmt"
-
-	"github.com/gocolly/colly/v2"
-)
-
-// CollyMain is the main function for the utility package.
-func CollyMain() {
-	c := colly.NewCollector()
-
-	// Find and visit all links
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		_ = e.Request.Visit(e.Attr("href"))
-	})
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
-
-	_ = c.Visit("https://go-colly.org/")
-}
+// import (
+// 	"fmt"
+//
+// 	"github.com/gocolly/colly/v2"
+// )
+//
+// // CollyMain is the main function for the utility package.
+// func CollyMain() {
+// 	c := colly.NewCollector()
+//
+// 	// Find and visit all links
+// 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+// 		_ = e.Request.Visit(e.Attr("href"))
+// 	})
+//
+// 	c.OnRequest(func(r *colly.Request) {
+// 		fmt.Println("Visiting", r.URL)
+// 	})
+//
+// 	_ = c.Visit("https://go-colly.org/")
+// }
--- internal/crawler/geziyor.go
+++ internal/crawler/geziyor.go
@@ -6,30 +6,23 @@
 
 package crawler
 
-import (
-	"github.com/PuerkitoBio/goquery"
-	"github.com/geziyor/geziyor"
-	"github.com/geziyor/geziyor/client"
-	"github.com/geziyor/geziyor/export"
-)
-
-// GeziyorMain is the main function for the utility package.
-func GeziyorMain() {
-	geziyor.NewGeziyor(&geziyor.Options{
-		StartURLs: []string{"https://quotes.toscrape.com/"},
-		ParseFunc: quotesParse,
-		Exporters: []export.Exporter{&export.JSON{}},
-	}).Start()
-}
-
-func quotesParse(g *geziyor.Geziyor, r *client.Response) {
-	r.HTMLDoc.Find("div.quote").Each(func(i int, s *goquery.Selection) {
-		g.Exports <- map[string]interface{}{
-			"text":   s.Find("span.text").Text(),
-			"author": s.Find("small.author").Text(),
-		}
-	})
-	if href, ok := r.HTMLDoc.Find("li.next > a").Attr("href"); ok {
-		g.Get(r.JoinURL(href), quotesParse)
-	}
-}
+// // GeziyorMain is the main function for the utility package.
+// func GeziyorMain() {
+// 	geziyor.NewGeziyor(&geziyor.Options{
+// 		StartURLs: []string{"https://quotes.toscrape.com/"},
+// 		ParseFunc: quotesParse,
+// 		Exporters: []export.Exporter{&export.JSON{}},
+// 	}).Start()
+// }
+//
+// func quotesParse(g *geziyor.Geziyor, r *client.Response) {
+// 	r.HTMLDoc.Find("div.quote").Each(func(i int, s *goquery.Selection) {
+// 		g.Exports <- map[string]interface{}{
+// 			"text":   s.Find("span.text").Text(),
+// 			"author": s.Find("small.author").Text(),
+// 		}
+// 	})
+// 	if href, ok := r.HTMLDoc.Find("li.next > a").Attr("href"); ok {
+// 		g.Get(r.JoinURL(href), quotesParse)
+// 	}
+// }
